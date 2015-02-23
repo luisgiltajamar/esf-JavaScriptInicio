5d22a2f21fb742a88ef6fc849348e15e58fd0c27
luisgiltajamar <luis.gil@tajamar365.com>
2015-02-23 17:23:54 +0100

    a
    Primera subida

---
+++ JavaScriptInicio/calcu.js
@@ -0,0 +1,47 @@
+﻿
+function sumar() {
+
+    var n1 = document.querySelector("#txtOp1");
+    var n2 = document.querySelector("#txtOp2");
+
+    if (!isNaN(n1.value) && !isNaN(n2.value)) {
+
+        var capa = document.querySelector("#resultado");
+        var r = parseInt(n1.value) + parseInt(n2.value);
+        capa.innerHTML = "La suma es " + r;
+
+    }
+    else {
+        alert("Solo se pueden sumar numeros");
+
+    }
+
+}
+function restar() {
+
+    var n1 = document.querySelector("#txtOp1");
+    var n2 = document.querySelector("#txtOp2");
+
+    if (!isNaN(n1.value) && !isNaN(n2.value)) {
+
+        var capa = document.querySelector("#resultado");
+        var r = parseInt(n1.value) - parseInt(n2.value);
+        capa.innerHTML = "La resta es " + r;
+
+    }
+    else {
+        alert("Solo se pueden sumar numeros");
+
+    }
+
+}
+
+(function () {
+    
+    var btMas = document.querySelector("#btnMas");
+    var btMenos = document.querySelector("#btnMenos");
+
+    btMas.addEventListener("click", sumar);
+    btMenos.addEventListener("click", restar);
+
+})();
---
+++ JavaScriptInicio/funciones.js
@@ -0,0 +1,42 @@
+﻿var n = 22;
+
+
+function calcularNumero() {
+   var  nn = 3;
+   alert(n * nn);
+
+   while (nn < 10) {
+        nn++;
+       if (nn % 5 == 0) {
+           document.write("Fin del while");
+           continue;
+       }
+       document.write(nn);
+     
+   }
+   do {
+       document.write("Soy do ");
+       document.write(nn);
+       nn++;
+   }while(nn<10);
+
+
+  // for(nn=3;nn<10;nn++)
+
+
+}
+function incrementarNumero() {
+    n += 12;
+   // n = n + 12;
+}
+function valorNn() {
+    alert(nn);
+
+}
+
+(function () {
+    incrementarNumero();
+    var bt=document.getElementById("miBoton");
+    bt.addEventListener("click", calcularNumero);
+   // alert(nn);
+})();
