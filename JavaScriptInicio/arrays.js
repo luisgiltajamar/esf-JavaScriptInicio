535c2ca191c333e483eef944460b89193ad17884
luisgiltajamar <luis.gil@tajamar365.com>
2015-02-23 18:20:40 +0100

    B

---
+++ JavaScriptInicio/arrays.js
@@ -0,0 +1,27 @@
+﻿var datos=[];
+
+function add() {
+    var tx = document.querySelector("#txtNombre").value;
+
+    datos.push(tx);
+
+    document.querySelector("#txtNombre").value = "";
+
+}
+function listar() {
+    
+    var res = document.querySelector("#resultado");
+    res.innerHTML = "";
+    for (var i = 0; i < datos.length; i++) {
+        res.innerHTML += datos[i] + "<br>";
+    }
+}
+(function () {
+    
+    var btnAdd = document.querySelector("#btnAdd");
+    var btnListar = document.querySelector("#btnListar");
+
+    btnAdd.addEventListener("click", add);
+    btnListar.addEventListener("click", listar);
+
+})();
